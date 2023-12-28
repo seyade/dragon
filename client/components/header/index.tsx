c53c068e7c76534d912e51aaacea078b33a6d1fd
Olu Adegun <oluseguna@apadmi.com>
2023-12-28 11:02:11 +0000

    started adding the user panel

--- client/components/header/index.tsx
+++ client/components/header/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import React from "react";
+import { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
 
 const Header = () => {
+	const [isUserPanelOpen, setIsUserPanelOpen] = useState(false);
 	const { user, isLoading } = useUser();
 
 	console.log("USER---", user);
+
+	const toggleUserPanel = () => {
+		setIsUserPanelOpen(!isUserPanelOpen);
+	};
 
 	return (
 		<header className="flex justify-between items-center w-full py-4 px-8 bg-zinc-50 border-b border-b-slate-200">
@@ -32,16 +37,29 @@ const Header = () => {
 
 				{user ? (
 					<>
-						<p className="font-semibold mr-2">{user?.name}</p>
-						{user?.picture && (
-							<Image
-								className="rounded-full"
-								width={48}
-								height={48}
-								src={user?.picture}
-								alt="User name"
-							/>
-						)}
+						<div className="relative">
+							<button
+								onClick={toggleUserPanel}
+								className="flex items-center hover:cursor-pointer"
+							>
+								<p className="font-semibold mr-2">{user?.name}</p>
+								{user?.picture && (
+									<Image
+										className="rounded-full"
+										width={48}
+										height={48}
+										src={user?.picture}
+										alt="User name"
+									/>
+								)}
+							</button>
+
+							{isUserPanelOpen && (
+								<div className="absolute bg-slate-400 p-3 rounded-xl">
+									User panel
+								</div>
+							)}
+						</div>
 
 						<a
 							className="flex items-center justify-center ml-4 py-2 px-4 bg-slate-700 text-white rounded-full"
