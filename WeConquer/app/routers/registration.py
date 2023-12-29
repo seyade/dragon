9afdcebe5b81f765341e8a62a0740fe063594d46
Ahmet Hilmi <ahmet.h@cycls.io>
2023-12-29 21:28:10 +0300

    Pricing table

--- WeConquer/app/routers/registration.py
+++ WeConquer/app/routers/registration.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 registration_router = APIRouter()
-@registration_router.get("/registration/")
-async def read_users():
+@registration_router.post("/registration/")
+async def read_users(request: Request):
+    incoming_json = {'fullName': 'Hilmi Terzi', 'sex': 'male', 'age': '25', 'country': 'USA', 'agreeToTerms': True}
+    #TODO Call db and save the data
+    #TODO User Status = Subscription
+    request = await request.json()
+    print(request)
     return [{"succesfull": 200}]
--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,7 +1,10 @@
 'use client'
-import React, {ChangeEvent, FormEvent, useState} from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useRouter } from 'next/navigation';
+
 
 const Register = () => {
+	const router = useRouter();
 	const [formData, setFormData] = useState({
 		fullName: '',
 		sex: '',
@@ -10,7 +13,7 @@ const Register = () => {
 		agreeToTerms: false,
 	});
 
-	const handleInputChange = (e) => {
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 		const { name, value, type, checked } = e.target;
 		setFormData({
 			...formData,
@@ -18,14 +21,14 @@ const Register = () => {
 		});
 	};
 
-	const handleSubmit = async (e) => {
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		if (!formData.agreeToTerms) {
 			alert("You must agree to the terms.");
 			return;
 		}
 		try {
-			const response = await fetch('YOUR_API_ENDPOINT', {
+			const response = await fetch('http://127.0.0.1:8000/registration/', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
@@ -37,6 +40,7 @@ const Register = () => {
 			}
 			const data = await response.json();
 			console.log(data);
+			router.push('/pricing')
 			// Handle success (redirect, show message, etc.)
 		} catch (error) {
 			console.error("Error during API call", error);
@@ -46,7 +50,7 @@ const Register = () => {
 
 	return (
 		<div className="w-full p-8">
-			<main className="flex flex-col items-center justify-between p-24">
+			<main className="flex flex-col items-center justify-between p-24 bg-white shadow-lg rounded-lg border border-gray-200">
 				<h1 className="font-bold text-4xl text-indigo-900 mb-8">Register</h1>
 				<form onSubmit={handleSubmit} className="w-full max-w-xs">
 					<input
@@ -55,13 +59,13 @@ const Register = () => {
 						placeholder="Full Name"
 						value={formData.fullName}
 						onChange={handleInputChange}
-						className="mb-4 p-2 w-full"
+						className="mb-4 p-2 w-full border-b-2 border-gray-300 focus:border-indigo-500 transition duration-300"
 					/>
 					<select
 						name="sex"
 						value={formData.sex}
 						onChange={handleInputChange}
-						className="mb-4 p-2 w-full">
+						className="mb-4 p-2 w-full border-b-2 border-gray-300 focus:border-indigo-500 transition duration-300">
 						<option value="">Select Sex</option>
 						<option value="male">Male</option>
 						<option value="female">Female</option>
@@ -73,21 +77,20 @@ const Register = () => {
 						placeholder="Age"
 						value={formData.age}
 						onChange={handleInputChange}
-						className="mb-4 p-2 w-full"
+						className="mb-4 p-2 w-full border-b-2 border-gray-300 focus:border-indigo-500 transition duration-300"
 					/>
 					<select
 						name="country"
 						value={formData.country}
 						onChange={handleInputChange}
-						className="mb-4 p-2 w-full">
+						className="mb-4 p-2 w-full border-b-2 border-gray-300 focus:border-indigo-500 transition duration-300">
 						<option value="">Select Country</option>
-						{/* Replace with your country list */}
 						<option value="USA">United States</option>
 						<option value="UK">United Kingdom</option>
 						<option value="Canada">Canada</option>
 						{/* Add other countries as needed */}
 					</select>
-					<label className="flex items-center">
+					<label className="flex items-center mb-4">
 						<input
 							type="checkbox"
 							name="agreeToTerms"
@@ -97,7 +100,7 @@ const Register = () => {
 						/>
 						Agree to terms and conditions
 					</label>
-					<button type="submit" className="bg-indigo-500 text-white p-2 w-full mt-4">Register</button>
+					<button type="submit" className="bg-indigo-500 hover:bg-indigo-600 text-white p-2 w-full mt-4 transition duration-300">Register</button>
 				</form>
 			</main>
 		</div>
