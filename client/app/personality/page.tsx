4d93c538386c237223a1402317956a4afb947377
Olu Adegun <oluseguna@apadmi.com>
2024-01-02 11:50:51 +0000

    added a timer

---
+++ client/app/personality/page.tsx
@@ -0,0 +1,12 @@
+import TestOne from "@/components/personality-tests/test-one";
+import React from "react";
+
+const Personaliy = () => {
+	return (
+		<section className="h-screen grid place-content-center bg-slate-900">
+			<TestOne />
+		</section>
+	);
+};
+
+export default Personaliy;
