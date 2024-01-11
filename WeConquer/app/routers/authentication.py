5bb11d86cb7c209abec39e0f20cce392cc1943ef
terzi <hilmiterzi46@gmail.com>
2024-01-12 00:51:51 +0300

    jjkh

--- WeConquer/app/routers/authentication.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-authentication_router = APIRouter()
-
-
-@authentication_router.get("/authentication/")
-async def read_users():
-    return [{"succesfull": 200}]
--- WeConquer/app/routers/payment.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter, Request
-from db import db
-
-payment_router = APIRouter()
-@payment_router.post("/verify_payment/")
-async def verify_payment(request: Request):
-    return None
